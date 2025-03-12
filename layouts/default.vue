fc7a413d25b28cffb8c6928c4a2e0613036ac2d8
Krenjeroth <krenjeroth.dev@gmail.com>
2025-03-12 11:15:46 +0800

    Create default.vue

---
+++ layouts/default.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const route = useRoute();
+
+const authPages = ["auth-login"];
+const isAuthPage = computed(() => authPages.includes(route.name as string));
+</script>
+
+<template>
+  <div class="min-h-screen flex">
+    <div class="flex-1 flex flex-col">
+      <!-- Header -->
+      <BaseAppHeader v-if="!isAuthPage" />
+
+      <div class="flex-1 flex">
+        <!-- Sidebar -->
+        <BaseAppSidebar v-if="!isAuthPage" />
+        <!-- Page Content -->
+        <main class="flex-1 p-4 dark:bg-gray-900">
+          <slot />
+        </main>
+      </div>
+    </div>
+  </div>
+</template>
