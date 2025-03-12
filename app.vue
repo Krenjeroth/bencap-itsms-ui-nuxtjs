ead471b734f0aa04447dbbeb4bb6dd2c5b7870dc
Krenjeroth <krenjeroth.dev@gmail.com>
2025-03-12 11:03:03 +0800

    Update app.vue

--- app.vue
+++ app.vue
@@ -1,6 +1,23 @@
+<script setup>
+const title =
+  "Benguet Capitol Information Technology Services Management System";
+const description =
+  "Benguet Capitol Information Technology Services Management System Description.";
+
+useHead({
+  titleTemplate: (titleChunk) => {
+    return titleChunk ? `${titleChunk} - ${title}` : title;
+  },
+});
+</script>
+
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+
+    <UModals />
+    <UNotifications />
   </div>
 </template>
