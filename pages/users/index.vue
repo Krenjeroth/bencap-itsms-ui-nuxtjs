ab5a0640bc49c6dc79478138c3d3a815434c6142
Krenjeroth <krenjeroth.dev@gmail.com>
2025-03-13 15:33:48 +0800

    Update index.vue

--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,3 +1,18 @@
+<script setup lang="ts">
+definePageMeta({
+  // middleware: ["sanctum:auth", "permission"],
+  title: "Users",
+  permission: "user_index",
+});
+
+useHead({
+  title: "Users",
+});
+
+const userStore = useUserStore();
+userStore.fetchUsers();
+</script>
+
 <template>
   <h1>Users</h1>
 </template>
