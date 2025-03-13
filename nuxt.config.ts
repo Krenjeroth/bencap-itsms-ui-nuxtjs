f777946acbac37211b32f0769d65061086889755
Krenjeroth <krenjeroth.dev@gmail.com>
2025-03-13 11:44:17 +0800

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-03-12",
   devtools: { enabled: false },
   ssr: false,
-  css: ["~/assets/main.scss"],
+  css: ["~/assets/styles/main.scss"],
   app: {
     head: {
       charset: "utf-8",
