619937e6078569164d95970b3d209f1c8ed6cd91
Krenjeroth <krenjeroth.dev@gmail.com>
2025-03-13 14:36:19 +0800

    Create useStringHandler.ts
    Added composable for string manipulations.

---
+++ composables/utils/useStringHandler.ts
@@ -0,0 +1,24 @@
+export const useStringHandler = () => {
+  const strSanitize = (str: string | undefined | null) => {
+    return str?.replace(/\s+/g, " ").trim().toLowerCase() || "";
+  };
+
+  const capitalizeWord = (str: string | undefined | null) => {
+    str = strSanitize(str);
+    return str?.replace(/^\w/, (c) => c.toUpperCase()) || "";
+  };
+
+  const capitalizeWords = (str: string | undefined | null) => {
+    str = strSanitize(str);
+    return (
+      str?.toLowerCase().replace(/\b\w/g, (char) => char.toUpperCase()) || ""
+    );
+  };
+
+  const capitalizeAll = (str: string | undefined | null) => {
+    str = strSanitize(str);
+    return str?.toUpperCase() || "";
+  };
+
+  return { capitalizeWord, capitalizeWords, capitalizeAll };
+};
