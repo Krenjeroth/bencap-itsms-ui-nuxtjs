35095e4db5ea1d69d4766c36647a9c6ecec9f695
Krenjeroth <krenjeroth.dev@gmail.com>
2025-03-19 10:12:39 +0800

    Create Modal.vue

---
+++ components/base/Modal.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+defineProps({
+  title: {
+    type: String,
+    default: "",
+  },
+  onClose: {
+    type: Function as PropType<IModalClose>,
+    default: null,
+  },
+});
+</script>
+
+<template>
+  <UModal prevent-close>
+    <UCard>
+      <template #header>
+        <div class="flex items-center justify-between">
+          <h3
+            class="text-base font-semibold leading-6 text-gray-900 dark:text-white"
+          >
+            {{ title }}
+          </h3>
+          <UButton
+            color="gray"
+            variant="ghost"
+            icon="i-heroicons-x-mark-20-solid"
+            class="-my-1"
+            @click="onClose"
+          />
+        </div>
+      </template>
+      <slot />
+    </UCard>
+  </UModal>
+</template>
