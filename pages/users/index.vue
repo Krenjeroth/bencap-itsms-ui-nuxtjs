03cd3f4b43c7513bda69b66f61783deb9e4c3349
Krenjeroth <krenjeroth.dev@gmail.com>
2025-03-19 10:12:26 +0800

    Update index.vue

--- pages/users/index.vue
+++ pages/users/index.vue
@@ -10,10 +10,55 @@ useHead({
   title: "Users",
 });
 
+import { UsersCreateModal } from "#components";
+import * as model from "./model/index";
+const modal = useModal();
+// const {actionToastResult} = useToastHandler();
+
 const userStore = useUserStore();
+const { users: data, loading } = storeToRefs(userStore);
+const { columns, items, statusOptions } = model;
 userStore.fetchUsers();
+
+const addUserModal = () => {
+  console.log("addUserModal");
+  modal.open(UsersCreateModal, {
+    onReloadTable() {
+      userStore.fetchUsers();
+    },
+    onSuccess() {
+      // actionToastResult({
+      //   icon: "i-heroicons-check-circle",
+      //   // title: "Success !",
+      //   description: "User created successfully.",
+      //   id: "modal-success",
+      //   color: "green",
+      // });
+    },
+    onError() {
+      // actionToastResult({
+      //   icon: "i-heroicons-x-circle",
+      //   // title: "Error !",
+      //   description: "Something went wrong.",
+      //   id: "modal-error",
+      //   color: "red",
+      // });
+    },
+    onClose() {
+      modal.close();
+    },
+  });
+};
 </script>
 
 <template>
-  <h1>Users</h1>
+  <div>
+    <UiDatatable
+      :module-title="'Users'"
+      :columns="columns"
+      :table-data="data"
+      :loading="loading"
+      :add-data-modal="addUserModal"
+    />
+  </div>
 </template>
