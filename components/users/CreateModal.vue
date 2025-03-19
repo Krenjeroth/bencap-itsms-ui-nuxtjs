b9a7ddb79945ec5c754abf7995f8664004ab9a5d
Krenjeroth <krenjeroth.dev@gmail.com>
2025-03-19 10:12:37 +0800

    Create CreateModal.vue

---
+++ components/users/CreateModal.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+const userStore = useUserStore();
+const { loading, errorBag, hasError } = storeToRefs(userStore);
+const emit = defineEmits(["reloadTable", "success", "error", "close"]);
+
+const onClose = () => emit("close");
+
+const onSuccess = () => {
+  emit("success");
+  emit("reloadTable");
+  onClose();
+};
+
+const onError = () => {
+  emit("error");
+};
+
+const formState = ref<ICreateUserForm>({
+  name: undefined,
+  email: undefined,
+  password: undefined,
+});
+
+const handleSubmit = async (
+  event: IFormSubmitEvent<TCreateUserValidationSchema>
+) => {
+  await userStore.addUser(event.data);
+
+  if (hasError.value) {
+    onError();
+    return;
+  }
+
+  onSuccess();
+  return;
+};
+</script>
+
+<template>
+  <BaseModal :on-close="onClose" :title="'Create User'">
+    <!-- <UForm
+      :schema="CreateUserValidationSchema"
+      :state="formState"
+      @submit.prevent="handleSubmit"
+      class="space-y-6"
+    >
+      <UFormGroup label="Name" name="name" :error="errorBag.name">
+        <UInput v-model="formState.name" />
+      </UFormGroup>
+
+      <UFormGroup label="Email" name="email" :error="errorBag.email">
+        <UInput v-model="formState.email" />
+      </UFormGroup>
+
+      <UFormGroup label="Password" name="password" :error="errorBag.password">
+        <UInput v-model="formState.password" type="password" />
+      </UFormGroup>
+
+      <UButton type="submit" :loading="loading"> Add </UButton>
+    </UForm> -->
+    Create User Modal
+  </BaseModal>
+</template>
