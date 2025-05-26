8a5d2b14bb4d9afa118ba00005bd968591f101ee
Krenjeroth <blizzmuth@gmail.com>
2025-05-26 19:50:52 +0800

    Create departments.ts

---
+++ stores/departments.ts
@@ -0,0 +1,83 @@
+export const useDepartmentStore = defineStore("departmentStore", () => {
+  const { fetchDepartmentsApi, addDepartmentApi } = useDepartmentApi();
+  const { hasError, errorBag, transformValidationErrors, resetErrorBag } =
+    useErrorHandler();
+  const { capitalizeWords } = useStringHandler();
+  // const { transformUtcDatetime } = useDateHandler();
+  enum SortDirection {
+    ASC = "asc",
+    DESC = "desc",
+  }
+  const departments = ref([]);
+  const loading = ref(false);
+  const page = ref(1);
+  const pageCount = ref(5);
+  const search = ref("");
+  const sort = ref<{ column: string; direction: SortDirection }>({
+    column: "created_at",
+    direction: SortDirection.DESC,
+  });
+  const totalDepartments = ref(0);
+  const selectedStatus = ref("");
+
+  const fetchDepartments = async () => {
+    loading.value = true;
+    try {
+      const queryParams = new URLSearchParams({
+        page: page.value.toString(),
+        per_page: pageCount.value.toString(),
+        sort: sort.value.column,
+        order: sort.value.direction,
+        search: search.value,
+        ...(selectedStatus.value ? { status: selectedStatus.value } : {}),
+      });
+
+      const response = await fetchDepartmentsApi(queryParams);
+
+      departments.value = response.data;
+      totalDepartments.value = Number(response.meta.total) || 0;
+    } catch (err: any) {
+      throw err;
+    } finally {
+      loading.value = false;
+    }
+  };
+
+  const addDepartment = async (form: ICreateDepartmentForm) => {
+    loading.value = true;
+    resetErrorBag();
+
+    const formattedForm = {
+      ...form,
+      name: capitalizeWords(form.name),
+      full_name: capitalizeWords(form.full_name),
+      division: capitalizeWords(form.division),
+      abbreviation: capitalizeWords(form.abbreviation),
+    };
+
+    await addDepartmentApi(formattedForm)
+      .catch((err) => {
+        transformValidationErrors(err);
+      })
+      .finally(() => {
+        loading.value = false;
+      });
+  };
+
+  return {
+    departments,
+    loading,
+    errorBag,
+    hasError,
+    page,
+    pageCount,
+    search,
+    sort,
+    totalDepartments,
+    selectedStatus,
+    fetchDepartments,
+    addDepartment,
+    // updateUser,
+    // deleteUser,
+  };
+});
