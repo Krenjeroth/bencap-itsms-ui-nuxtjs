846b33f257ae46cdbb197a1d17fe0701e1d6f4d9
Krenjeroth <blizzmuth@gmail.com>
2026-03-23 10:30:21 +0800

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2025-03-12",
+  compatibilityDate: "2026-01-01",
   devtools: { enabled: false },
   ssr: false,
   css: ["~/assets/styles/main.scss"],
