f733f864be625fd31c0218178f20dfd16473bf5f
Krenjeroth <blizzmuth@gmail.com>
2026-04-28 14:23:35 +0800

    Update UpdateUserValidationSchema.ts

--- utils/schemas/user/UpdateUserValidationSchema.ts
+++ utils/schemas/user/UpdateUserValidationSchema.ts
@@ -1,6 +1,7 @@
 import { z } from "zod";
+
 const acceptedFileTypes = ["image/png", "image/jpeg", "image/jpg"];
-const fileSizeLimit = 2 * 1024 * 1024; // 2MB
+const fileSizeLimit = 2 * 1024 * 1024;
 
 export const UpdateUserValidationSchema = z
   .object({
@@ -9,55 +10,61 @@ export const UpdateUserValidationSchema = z
         invalid_type_error: "Email is required",
       })
       .email("Email is not valid"),
-    role: z.union([z.number(), z.string()]),
-    offices_assigned_ids: z
-      .array(z.number())
-      .min(1, "At least one office is required"),
-    agencies_assigned_ids: z
-      .array(z.number())
-      .min(1, "At least one agency is required"),
-    // Profile
+
+    role: z.coerce.number(),
+
+    offices_assigned_ids: z.array(z.coerce.number()).optional(),
+
+    agencies_assigned_ids: z.array(z.coerce.number()).optional().default([]),
+
     prefix: z
       .string()
       .regex(/^[A-Za-z]+$/, "Prefix must be alphabets only.")
       .nullable()
       .optional(),
+
     firstname: z
       .string({
         required_error: "First name is required",
       })
       .min(3, "First name must be at least 3 characters long")
       .max(50, "First name must be at most 50 characters long"),
+
     middlename: z
       .string({
         required_error: "Middle name is required",
       })
       .min(3, "Middle name must be at least 3 characters long")
       .max(50, "Middle name must be at most 50 characters long"),
+
     lastname: z
       .string({
         required_error: "Last name is required",
       })
       .min(3, "Last name must be at least 3 characters long")
       .max(50, "Last name must be at most 50 characters long"),
+
     suffix: z
       .string()
       .regex(/^[A-Za-z]+$/, "Suffix must be alphabets only.")
       .nullable()
       .optional(),
+
     designation: z
       .string({
         required_error: "Designation is required",
       })
       .min(3, "Designation must be at least 3 characters long")
       .max(50, "Designation must be at most 50 characters long"),
+
     gender: z.enum(["male", "female"]),
+
     photo_id: z
       .custom<File | undefined | null>()
-      .refine((file) => !file || acceptedFileTypes.includes(file?.type)!, {
+      .refine((file) => !file || acceptedFileTypes.includes(file?.type), {
         message: "Photo ID must be a PNG, JPEG, or JPG",
       })
-      .refine((file) => !file || file?.size <= fileSizeLimit!, {
+      .refine((file) => !file || file.size <= fileSizeLimit, {
         message: "Photo ID must be less than 2MB",
       }),
   })
@@ -89,9 +96,9 @@ export const UpdateUserValidationSchema = z
 
 export const updatePhotoIdSchema = z
   .custom<File | undefined | null>()
-  .refine((file) => !file || acceptedFileTypes.includes(file?.type)!, {
+  .refine((file) => !file || acceptedFileTypes.includes(file?.type), {
     message: "Photo ID must be a PNG, JPEG, or JPG",
   })
-  .refine((file) => !file || file?.size <= fileSizeLimit!, {
+  .refine((file) => !file || file.size <= fileSizeLimit, {
     message: "Photo ID must be less than 2MB",
   });
