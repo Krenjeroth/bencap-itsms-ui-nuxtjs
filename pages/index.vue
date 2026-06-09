ac3fb88e31de33c1074e59d2259f8ab8da690583
Krenjeroth <blizzmuth@gmail.com>
2026-06-09 11:37:54 +0800

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
+const { user } = useSanctumAuth<IUser>();
+
 definePageMeta({
   middleware: ["sanctum:auth"],
   title: "Dashboard",
-  // permission: "user_index",
 });
 
 useHead({
@@ -11,8 +12,73 @@ useHead({
 </script>
 
 <template>
-  <div>
-    <h1>Pages Index</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+  <div class="orbit-container">
+    <div class="center-point"></div>
+
+    <div class="orbit-path">
+      <img
+        :src="user?.profile?.img_path"
+        alt="Orbiting Image"
+        class="orbiting-image"
+      />
+    </div>
   </div>
 </template>
+
+<style scoped>
+.orbit-container {
+  position: relative;
+  width: 420px;
+  height: 420px;
+  margin: 100px auto;
+  border: 1px dashed rgba(255, 255, 255, 0.3);
+  border-radius: 50%;
+}
+
+.center-point {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 12px;
+  height: 12px;
+  background: #00dc82;
+  border-radius: 50%;
+  transform: translate(-50%, -50%);
+}
+
+.orbit-path {
+  position: absolute;
+  inset: 0;
+  animation: orbit 4s linear infinite;
+}
+
+.orbiting-image {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 170px;
+  height: 170px;
+  border-radius: 50%;
+  object-fit: cover;
+  transform: translate(-50%, -50%) translateX(125px);
+  animation: keep-upright 4s linear infinite;
+}
+
+@keyframes orbit {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+
+@keyframes keep-upright {
+  from {
+    transform: translate(-50%, -50%) translateX(125px) rotate(0deg);
+  }
+  to {
+    transform: translate(-50%, -50%) translateX(125px) rotate(-360deg);
+  }
+}
+</style>
